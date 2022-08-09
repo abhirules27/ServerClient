6fd86ae9f11bc1fc0cd54c0a2f1fd6df0ef6e390
Abhishek Mishra <abhirules27@gmail.com>
2022-08-09 19:24:00 +0530

    Started Coding Struggling with Auto-py-to-EXE

--- Client.py
+++ Client.py
@@ -1,5 +1,17 @@
 # Client Codee
 
 # To be converted in exe
-# Changed from MAC
-# Now changed from GitHub
+import socket
+
+SERVER_IP = '192.168.0.181'
+SERVER_PORT = 5678
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((SERVER_IP, SERVER_PORT))
+    data = s.recv(1024)
+    print(data)
+
+input() // To
+stay
+on
+Screen
--- Server.py
+++ Server.py
@@ -1,5 +1,39 @@
 # Server Code
 
 # Learning Socket Module
-# Changed from MAC
-# Now changed from GitHub
+import socket
+
+SERVER_IP = '192.168.0.200'
+SERVER_PORT = 5678
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    //
+    AF_INET
+means
+IPv4.SOCK_STREAM
+means
+protocol
+TCP.
+// For
+UDP, use
+SOCK_DGRAM
+s.bind((SERVER_IP, SERVER_PORT))
+print("Server Listening...")
+s.listen(1) // This
+will
+listen
+for One Connection
+    // This
+    will
+    stop
+    code
+    exec, till
+    it
+    gets
+    Connection
+conn, addr = s.accept()
+print(f'Connection estb from: {addr}')
+
+with conn:
+    while True:
+        conn.send(b'Hello World!')
+        break
