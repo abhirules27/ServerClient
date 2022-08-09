b272117686b971f13a246c6c4f34dd60ed7eaeb2
Abhishek Mishra <abhirules27@gmail.com>
2022-08-09 19:24:42 +0530

    Started Coding Struggling with Auto-py-to-EXE

--- Client.py
+++ Client.py
@@ -2,16 +2,12 @@
 
 # To be converted in exe
 import socket
-
-SERVER_IP = '192.168.0.181'
-SERVER_PORT = 5678
+SERVER_IP='192.168.0.181'
+SERVER_PORT=5678
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((SERVER_IP, SERVER_PORT))
+    s.connect((SERVER_IP,SERVER_PORT))
     data = s.recv(1024)
     print(data)
 
-input() // To
-stay
-on
-Screen
+input()                         // To stay on Screen
